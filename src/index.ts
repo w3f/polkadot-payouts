1e67bbb82904177272e638a936a7461f9412953f
Federico Gimenez <federico.gimenez@gmail.com>
2020-03-25 20:13:04 +0100

    e2e-tests command

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import process from 'process';
 import program from 'commander';
 
 import { startAction } from './actions/start';
-
+import { startE2ETestsAction } from './actions/e2e-tests';
 
 program
     .command('start')
     .description('Starts the accountant.')
     .option('-c, --config [path]', 'Path to config file.', './config/main.yaml')
     .action(startAction);
+
+program
+    .command('e2e-tests')
+    .description('Runs e2e tests.')
+    .option('-c, --config [path]', 'Path to config file.', './config/main.yaml')
+    .action(startE2ETestsAction);
 
 program.allowUnknownOption(false)
 
