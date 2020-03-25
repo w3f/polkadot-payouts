d5e4b4683811c85e2f71629bd606a69f3a311fed
Federico Gimenez <federico.gimenez@gmail.com>
2020-03-25 12:10:32 +0100

    no need to track tx status

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,6 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Keyring } from '@polkadot/keyring';
 import { Balance } from '@polkadot/types/interfaces'
-import { createType, GenericImmortalEra } from '@polkadot/types';
 import { waitReady } from '@polkadot/wasm-crypto';
 import fs from 'fs-extra';
 
@@ -32,35 +31,14 @@ export class Client {
             await this.connect();
         }
 
-        const era = createType(
-            this.api.registry,
-            'ExtrinsicEra',
-            new GenericImmortalEra(this.api.registry)
-        );
-
         const keyring = new Keyring({ type: 'sr25519' });
         const keyContents = fs.readFileSync(keystore.filePath, { encoding: 'utf-8' });
         const senderKeyPair = keyring.addFromJson(JSON.parse(keyContents));
         const passwordContents = fs.readFileSync(keystore.passwordPath, { encoding: 'utf-8' });
         senderKeyPair.decodePkcs8(passwordContents);
 
-        const account = await this.getAccount(senderKeyPair.address);
-
-        const transfer = this.api.tx.balances.transfer(recipentAddress, amount);
-        const transferOptions = {
-            blockHash: this.api.genesisHash,
-            era,
-            nonce: account.nonce
-        };
-        try {
-            await transfer.signAndSend(
-                senderKeyPair,
-                transferOptions,
-                this.sendStatusCb.bind(this)
-            );
-        } catch (e) {
-            this.logger.info(`Exception during tx sign and send: ${e}`);
-        }
+        await this.api.tx.balances
+            .transfer(recipentAddress, amount).signAndSend(senderKeyPair);
     }
 
     private async connect() {
@@ -83,30 +61,5 @@ export class Client {
 
     private async getAccount(addr: string) {
         return this.api.query.system.account(addr);
-    }
-
-    private async sendStatusCb({ events = [], status }) {
-        switch (status.type) {
-            case 'Invalid':
-                this.logger.info(`Transaction invalid`);
-                break;
-            case 'Ready':
-                this.logger.info(`Transaction is ready`);
-                break;
-            case 'Broadcast':
-                this.logger.info(`Transaction has been broadcasted`);
-                break;
-            case 'Finalized':
-                this.logger.info(`Transaction has been included in blockHash ${status.asFinalized}`);
-                events.forEach(
-                    async ({ event: { method } }) => {
-                        if (method === 'ExtrinsicSuccess') {
-                            this.logger.info(`Transaction succeeded`);
-                        } else if (method === 'ExtrinsicFailed') {
-                            this.logger.info(`Transaction failed`);
-                        }
-                    }
-                );
-        }
     }
 }
